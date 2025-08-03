c30744a466a3e7901e3ed473c54e59eb023a4e64
arthakar <arthakar@terpmail.umd.edu>
2025-08-03 14:27:14 -0400

    Add app.py hello world

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, flash, redirect, request
+import git
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+    # return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return 'This is about us'
+
+@app.route("/search")
+def search():
+    query = request.args.get('q', '')
+    # For now, just return a simple message
+    # In a real app, you would search your database here
+    if query:
+        return f'Search results for: {query}'
+    else:
+        return 'Please enter a search term'
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/flaskdeploy1/flask-deploy-1')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+if __name__ == '__main__':
+    app.run()
